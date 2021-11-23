c166ee1cd02ffe9ffb6d5818711a72ad5addf454
Klim <Severin.Klim@yandex.by>
2021-11-24 02:17:01 +0300

    Added consoleApp

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnitTestsGeneratorLibrary;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GeneratorConfig generatorConfig = new GeneratorConfig
+            {
+                EndpointFolder = @"D:\Projects\SPP\UnitTestsGenerator\SampleLibrary.Tests\",
+                MaxParallelLoadCount = 1,
+                MaxParallelExecCount = 1,
+                MaxParallelWriteCount = 1
+            };
+            generatorConfig.Filenames.Add(@"D:\Projects\SPP\UnitTestsGenerator\SampleLibrary\ClassA.cs");
+            generatorConfig.Filenames.Add(@"D:\Projects\SPP\UnitTestsGenerator\SampleLibrary\ClassB.cs");
+            IUnitTestsGenerator unitTestsGenerator = new UnitTestsGenerator();
+            unitTestsGenerator.GenerateTests(generatorConfig).Wait();
+        }
+    }
+}
